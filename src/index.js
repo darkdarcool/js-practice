7c1060d15cb641cedea2d2c2c461bc4b4280bd26
darkdarcool <darrendreddy@gmail.com>
2021-04-07 14:01:36 -0400

    hiya pal
    how are you?

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 class make {
-	constructor(value) {
-		this.list = value;
-	}
-	createlist(type) {
-		if (type == '--strong') {
-			var stuff = this.list.split("");
-			return stuff;
-		}
-		if (type == '--weak') {
-			var stuff = this.list.split(" ")
-			return stuff
-		} 
-		else {
-			throw new Error(`${type} is not a valid flag`)
-		}
-	}
-	createstring() {
-		var stuff = this.list;
-		return stuff.join("");
-	}
+    constructor(value) {
+        this.list = value;
+    }
+    createlist(type) {
+        if (type == '--strong') {
+            var stuff = this.list.split("");
+            return stuff;
+        }
+        if (type == '--weak') {
+            var stuff = this.list.split(" ")
+            return stuff
+        } 
+        else {
+            throw new Error(`${type} is not a valid flag`)
+        }
+    }
+    createstring() {
+        var stuff = this.list;
+        return stuff.join("");
+    }
+    getelem() {
+        this.test = Array.isArray(this.list)
+        if (this.test == true) {	
+            return this.list.length
+        }
+        else {
+            throw new Error(`${this.list} is not a list!`)
+        }
+    }
 }
-function getelem(list) {
-	test = Array.isArray(list)
-	if (test == true) {	
-		return list.length
-	}
-	else {
-		throw new Error(`${list} is not a list!`)
-	}
-}
+stuff = ['H', 'e', 'l', 'l', 'o', '!']
+let sun = new make(stuff)
 
-module.exports = getelem;
+console.log(mystuff)
+module.exports = make;
